68a97e40e9b8bad8f1171f8f5a3116dfb3722fd1
Pavan Kumar Sahu <140802616+Pavan7201@users.noreply.github.com>
2025-11-05 12:14:26 +0530

    Change API URL to relative path

--- frontend/src/components/Login2/index.js
+++ frontend/src/components/Login2/index.js
@@ -14,7 +14,7 @@ const Login = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:80/api/auth";
+			const url = "/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
 			window.location = "/";
