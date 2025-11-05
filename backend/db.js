f5c520d781474ba4e63b04b72c8cc3f71cb012e8
Pavan Kumar Sahu <140802616+Pavan7201@users.noreply.github.com>
2025-11-05 11:23:52 +0530

    Security risk removed

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
-const url =
-  "mongodb+srv://pavanUser:Prince@p7201@cluster0.drsmnvx.mongodb.net/?appName=Cluster0";
+const url = process.env.DATABASE_URL;
+
 module.exports.connect = () => {
+  // Exit if the database URL is not set
+  if (!url) {
+    console.error("Error: MongoDB connection string (DATABASE_URL) is not defined in your environment variables.");
+    process.exit(1);
+  }
+
   mongoose
     .connect(url, {
       useNewUrlParser: true,
-      // useFindAndModify: false,
       useUnifiedTopology: true,
-      // useCreateIndex: true,
     })
     .then(() => console.log("MongoDB is connected successfully"))
-    .catch((err) => console.log("Error: ", err));
+    .catch((err) => console.log("Error connecting to MongoDB: ", err));
 };
